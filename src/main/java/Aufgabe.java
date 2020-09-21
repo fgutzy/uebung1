ceaa49e6e1adcf22f617f4e9a1affd851874e58a
Bastian Katz <bastian.katz@hm.edu>
2020-09-21 09:33:21 +0200

    changed formatting to google style

--- src/main/java/Aufgabe.java
+++ src/main/java/Aufgabe.java
@@ -1,25 +1,27 @@
 import static edu.hm.cs.bka.hmkara.Kara.*;
 
 import edu.hm.cs.bka.hmkara.Kara;
-public class Aufgabe{
 
-	public static void main(String[] args) {
-		move();
-		int i = Kara.askNumber("Guess a Number");
-		turnRight();
-		move();
-		move();
-		move();
-		turnLeft();
-		move();
-		move();
-		turnLeft();
-		move();
-		turnRight();
-		move();
-		pickLeaf();
-		Kara.say("Yay " + i);
-	}
+public class Aufgabe {
 
-	
+  public static void main(String[] args) {
+    move();
+    int i = Kara.askNumber("Guess a Number");
+    turnRight();
+    move();
+    move();
+    move();
+    move();
+    turnLeft();
+    move();
+    move();
+    turnLeft();
+    move();
+    turnRight();
+    move();
+    pickLeaf();
+    Kara.say("Yay " + i);
+  }
+
+
 }
